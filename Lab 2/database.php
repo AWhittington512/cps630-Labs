01fef87281bdb888c1f290f14d6790322f6f7092
Peter Tran <petertranminh21@gmail.com>
2023-02-10 03:18:47 -0500

    update

--- Lab 2/database.php
+++ Lab 2/database.php
@@ -5,7 +5,7 @@ $password = "";
 $database = "testnew";
 $connection = mysqli_connect($host, $user, $password, $database);
 if (mysqli_connect_errno()) {
-    echo "Connection failed.";
+    echo "<h3>Connection failed.</h3>";
     die(mysqli_connect_error());
 } else {
     echo "<h3>Connection established.</h3>";
